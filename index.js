29575e4dd8b3cccde431aeaee9c8f96a4f94e1ab
Avnish <Avnishway@gmail.com>
2020-04-16 19:57:45 +0530

    Adding cookies functionality

--- index.js
+++ index.js
@@ -10,8 +10,28 @@ async function handleRequest(request) {
     "https://cfw-takehome.developers.workers.dev/api/variants"
   );
   const { variants } = await urls_res.json();
-  const sendIndex = Math.floor((Math.random() * 100) % 2);
-  let finalResponse = await fetch(variants[sendIndex]);
-  // finalResponse.headers["Set-Cookie"] = "test";
-  return finalResponse;
+  const sendVariant = getSendVariant(request, "prev_variant");
+  let finalResponse = await fetch(variants[sendVariant]);
+  let response = new Response(finalResponse.body);
+  response.headers.set(
+    "Set-Cookie",
+    `prev_variant=${sendVariant === 0 ? "first" : "second"}`
+  );
+  return response;
+}
+
+function getSendVariant(request, name) {
+  const cookieString = request.headers.get("Cookie");
+  let result = "second";
+  if (cookieString) {
+    let cookies = cookieString.split(";");
+    cookies.forEach((cookie) => {
+      let cookieName = cookie.split("=")[0].trim();
+      if (cookieName === name) {
+        let cookieVal = cookie.split("=")[1];
+        result = cookieVal;
+      }
+    });
+  }
+  return result === "second" ? 0 : 1;
 }
